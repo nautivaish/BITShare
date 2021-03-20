f2ca88ab5fb6ebebab669de179f80ffa3ba3b009
the-hyp0cr1t3 <hriday.gajulapalli@gmail.com>
2021-03-20 18:13:41 +0530

    refactor backend

--- routes/api/items.js
+++ routes/api/items.js
@@ -9,23 +9,18 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
-const {getItems, postItem, deleteItem, othersItems} = require("../../controllers/item");
+const { getItems, postItem, deleteItem, othersItems } = require("../../controllers/item");
 // const {getItems, postItem, deleteItem, othersItems, searchItems} = require("../../controllers/item");
 const express = require("express");
 const router = express.Router();
-const jwt = require("jsonwebtoken");
-const keys = process.env.SECRET_KEY;
-
-// Load User model
-const Item = require("../../models/Item");
 
 // @route POST api/users/register
 // @desc Register user
 // @access Public
-router.post("/postItem/:id",upload.single('image'), postItem);
-router.get("/getItems/:id",getItems);
-router.post("/deleteItem/:id",deleteItem);
-router.get("/othersItems/:id",othersItems);
-// router.get("searchItems/:val",searchItems);
+router.post("/postItem/:id", upload.single('image'), postItem);
+router.get("/getItems/:id", getItems);
+router.post("/deleteItem/:id", deleteItem);
+router.get("/othersItems/:id", othersItems);
+// router.get("searchItems/:val", searchItems);
 
 module.exports = router;
