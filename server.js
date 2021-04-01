d187652b328ac704132d53d67b4d02264dc38d32
21jjsk <21jjsk@gmail.com>
2021-04-01 14:59:15 +0530

    Heroku changes

--- server.js
+++ server.js
@@ -28,8 +28,6 @@ mongoose.connect(
 const path = require("path");
 app.use(express.static(path.join(__dirname, 'client', 'build'))); 
 
-
-
 app.use(passport.initialize());
 require("./config/passport")(passport);
 
