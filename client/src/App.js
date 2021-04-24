3eaa26b59a54841bfb55dea92ceeaee74028c304
unknown <vaishnauti@gmail.com>
2021-04-24 17:41:22 +0530

    changed UI of landing page

--- client/src/App.js
+++ client/src/App.js
@@ -30,7 +30,7 @@ import Requests from "./components/Request portal/Requests";
 import MyRequests from "./components/Request portal/MyRequests";
 import "./App.css";
 import CheckStatus from "./components/Request portal/CheckStatus";
-
+import AdminLogin from "./components/auth/AdminLogin";
 const TheLayout = React.lazy(() => import('./coreui-dashboard/src/containers/TheLayout'));
 // import TheLayout from './coreui-dashboard/src/containers/TheLayout';
 
@@ -67,8 +67,10 @@ class App extends Component {
           <div className="App">
             
             <Route exact path="/" component={Landing} />
+            <Route exact path="/adminlogin" component={AdminLogin} />
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
+            
             
             <React.Suspense fallback={loading}>
               <Route exact path="/admindashboard" component={TheLayout} />
@@ -99,42 +101,4 @@ class App extends Component {
 
 export default App;
 
-// import React, { Component } from 'react';
-// import { HashRouter, Route, Switch } from 'react-router-dom';
-// // import './scss/style.scss';
 
-// const loading = (
-//   <div className="pt-3 text-center">
-//     <div className="sk-spinner sk-spinner-pulse"></div>
-//   </div>
-// )
-
-// // Containers
-// const TheLayout = React.lazy(() => import('./coreui-dashboard/src/containers/TheLayout'));
-
-// // Pages
-// // const Login = React.lazy(() => import('./views/pages/login/Login'));
-// // const Register = React.lazy(() => import('./views/pages/register/Register'));
-// // const Page404 = React.lazy(() => import('./views/pages/page404/Page404'));
-// // const Page500 = React.lazy(() => import('./views/pages/page500/Page500'));
-
-// class App extends Component {
-
-//   render() {
-//     return (
-//       <HashRouter>
-//           <React.Suspense fallback={loading}>
-//             <Switch>
-//               {/* <Route exact path="/login" name="Login Page" render={props => <Login {...props}/>} />
-//               <Route exact path="/register" name="Register Page" render={props => <Register {...props}/>} />
-//               <Route exact path="/404" name="Page 404" render={props => <Page404 {...props}/>} />
-//               <Route exact path="/500" name="Page 500" render={props => <Page500 {...props}/>} /> */}
-//               <Route path="/" name="Home" render={props => <TheLayout {...props}/>} />
-//             </Switch>
-//           </React.Suspense>
-//       </HashRouter>
-//     );
-//   }
-// }
-
-// export default App;
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,58 +1,71 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import BPHCimg from "./BPHC_img.png";
-
+import BPHCimg from "./8.jpg";
+import frontimg from "./7.jpg"
+import admin from "./admin.png"
 class Landing extends Component {
   render() {
     return (
       <div style={{backgroundImage: "url("+BPHCimg+")", backgroundSize: "Cover", height:"100vh"}}>
-      <div
-        style={{height: "75vh"}}
-        className="container valign-wrapper">
-        <div className="row" style={{backgroundColor:"rgba(255,255,255,0.6)", padding: "20px", borderRadius:"25px"}}>
-          <div className="col s12 center-align">
-            <h1 style={{ margin: "0px" }}>BITShare</h1>
-            <p style={{margin: "40px 0px 70px 0px"}} className="flow-text black-text text-darken-2">
-              Sharing app of the Bitsians, by the Bitsians, for the Bitsians
-            </p>
-            <br />
-            <div className="col s6">
-              <Link
-                to="/register"
+        <div style={{alignItems:"right",position:"absolute",top:5,right:5}}>
+        <Link
+                to="/adminlogin"
                 style={{
-                  width: "140px",
+                  width: "60px",
                   borderRadius: "3px",
                   letterSpacing: "1.5px",
                 }}
-                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
               >
-                Register
+                        <img src={admin} style={{height:"50px", borderRadius:"50%"}}  />
+
+              </Link>
+        {/* <img src={admin} style={{height:"50px", borderRadius:"50%",position:"absolute",right:"5",top:"5", left:"50",bottom:"50"}} href="/admindashboard" /> */}
+        </div>
+        
+      <div
+        style={{width:"1200px",paddingTop:"100px",paddingLeft:"50px",paddingRight:"50px",height:"120vh"}}
+        className="container">
+        <div className="row" style={{backgroundImage: "url("+frontimg+")",backgroundSize: "Cover", height:"60%", padding: "40px", borderRadius:"25px"}}>
+          <div className="col s12 center-align">
+            <h1 style={{ margin: "0px",fontSize:"700%",color:"black"}}><b>BITShare</b></h1>
+            
+            <br />
+            <br/>
+            <br/>
+            <div className="col s6" style={{paddingTop:"60px"}}>
+              <Link
+                to="/register"
+                className="btn btn-large waves-effect waves-light hoverable  accent-3"
+                style={{
+                  width: "250px",
+                  // height:"60px",
+                  borderRadius: "5px",
+                  letterSpacing: "3px",
+                  backgroundColor:"#0000A0",
+                  fontSize:"130%",
+                                
+                }}
+                
+              >
+                <b>Register</b>
               </Link>
             </div>
-            <div className="col s6">
+            <div className="col s6" style={{paddingTop:"60px"}}>
               <Link
                 to="/login"
                 style={{
-                  width: "140px",
-                  borderRadius: "3px",
-                  letterSpacing: "1.5px",
+                  width: "250px",
+                  // height:"60px",
+                  borderRadius: "5px",
+                  letterSpacing: "3px",
+                  backgroundColor:"#0000A0",
+                  fontSize:"130%"
                 }}
-                className="btn btn-large waves-effect hoverable white black-text"
-              >
-                Log In
+                className="btn btn-large waves-effect waves-light hoverable  accent-3"
+              ><b>Log In</b>
+                
               </Link>
             </div>
-            <Link
-                to="/admindashboard"
-                style={{
-                  width: "140px",
-                  borderRadius: "3px",
-                  letterSpacing: "1.5px",
-                }}
-                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-              >
-                Admin Dashboard
-              </Link>
           </div>
           </div>
         </div>
